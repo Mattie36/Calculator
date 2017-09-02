6bd120dab73fc670aa7f3dacb08472e74acdaccd
Mattie36 <mesaia907@gmail.com>
2017-09-02 12:54:32 -0400

    Calculator

--- Calculator/Calculator/Form1.Designer.vb
+++ Calculator/Calculator/Form1.Designer.vb
@@ -24,28 +24,302 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Plus = New System.Windows.Forms.Button()
+        Me.One = New System.Windows.Forms.Button()
+        Me.Two = New System.Windows.Forms.Button()
+        Me.Five = New System.Windows.Forms.Button()
+        Me.Four = New System.Windows.Forms.Button()
+        Me.Eight = New System.Windows.Forms.Button()
+        Me.Nine = New System.Windows.Forms.Button()
+        Me.Six = New System.Windows.Forms.Button()
+        Me.Three = New System.Windows.Forms.Button()
+        Me.Square = New System.Windows.Forms.Button()
+        Me.SquareRoot = New System.Windows.Forms.Button()
+        Me.Seven = New System.Windows.Forms.Button()
+        Me.Exponent = New System.Windows.Forms.Button()
+        Me.Roots = New System.Windows.Forms.Button()
+        Me.Zero = New System.Windows.Forms.Button()
+        Me.Point = New System.Windows.Forms.Button()
+        Me.Negative = New System.Windows.Forms.Button()
+        Me.Solve = New System.Windows.Forms.Button()
+        Me.Divide = New System.Windows.Forms.Button()
+        Me.Multiply = New System.Windows.Forms.Button()
+        Me.Minus = New System.Windows.Forms.Button()
+        Me.Clear = New System.Windows.Forms.Button()
+        Me.Backspace = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(310, 70)
+        Me.TextBox1.Location = New System.Drawing.Point(377, 70)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(366, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(299, 20)
         Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "0"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
         'TextBox2
         '
         Me.TextBox2.Enabled = False
-        Me.TextBox2.Location = New System.Drawing.Point(310, 44)
+        Me.TextBox2.Location = New System.Drawing.Point(377, 44)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(366, 20)
+        Me.TextBox2.Size = New System.Drawing.Size(299, 20)
         Me.TextBox2.TabIndex = 1
+        Me.TextBox2.Text = "0"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Plus
+        '
+        Me.Plus.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Plus.Location = New System.Drawing.Point(621, 96)
+        Me.Plus.Name = "Plus"
+        Me.Plus.Size = New System.Drawing.Size(55, 55)
+        Me.Plus.TabIndex = 2
+        Me.Plus.Text = "+"
+        Me.Plus.UseVisualStyleBackColor = True
+        '
+        'One
+        '
+        Me.One.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.One.Location = New System.Drawing.Point(438, 157)
+        Me.One.Name = "One"
+        Me.One.Size = New System.Drawing.Size(55, 55)
+        Me.One.TabIndex = 3
+        Me.One.Text = "1"
+        Me.One.UseVisualStyleBackColor = True
+        '
+        'Two
+        '
+        Me.Two.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Two.Location = New System.Drawing.Point(499, 157)
+        Me.Two.Name = "Two"
+        Me.Two.Size = New System.Drawing.Size(55, 55)
+        Me.Two.TabIndex = 4
+        Me.Two.Text = "2"
+        Me.Two.UseVisualStyleBackColor = True
+        '
+        'Five
+        '
+        Me.Five.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Five.Location = New System.Drawing.Point(499, 218)
+        Me.Five.Name = "Five"
+        Me.Five.Size = New System.Drawing.Size(55, 55)
+        Me.Five.TabIndex = 5
+        Me.Five.Text = "5"
+        Me.Five.UseVisualStyleBackColor = True
+        '
+        'Four
+        '
+        Me.Four.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Four.Location = New System.Drawing.Point(438, 218)
+        Me.Four.Name = "Four"
+        Me.Four.Size = New System.Drawing.Size(55, 55)
+        Me.Four.TabIndex = 6
+        Me.Four.Text = "4"
+        Me.Four.UseVisualStyleBackColor = True
+        '
+        'Eight
+        '
+        Me.Eight.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Eight.Location = New System.Drawing.Point(499, 279)
+        Me.Eight.Name = "Eight"
+        Me.Eight.Size = New System.Drawing.Size(55, 55)
+        Me.Eight.TabIndex = 7
+        Me.Eight.Text = "8"
+        Me.Eight.UseVisualStyleBackColor = True
+        '
+        'Nine
+        '
+        Me.Nine.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Nine.Location = New System.Drawing.Point(560, 279)
+        Me.Nine.Name = "Nine"
+        Me.Nine.Size = New System.Drawing.Size(55, 55)
+        Me.Nine.TabIndex = 8
+        Me.Nine.Text = "9"
+        Me.Nine.UseVisualStyleBackColor = True
+        '
+        'Six
+        '
+        Me.Six.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Six.Location = New System.Drawing.Point(560, 220)
+        Me.Six.Name = "Six"
+        Me.Six.Size = New System.Drawing.Size(55, 55)
+        Me.Six.TabIndex = 9
+        Me.Six.Text = "6"
+        Me.Six.UseVisualStyleBackColor = True
+        '
+        'Three
+        '
+        Me.Three.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Three.Location = New System.Drawing.Point(560, 157)
+        Me.Three.Name = "Three"
+        Me.Three.Size = New System.Drawing.Size(55, 55)
+        Me.Three.TabIndex = 10
+        Me.Three.Text = "3"
+        Me.Three.UseVisualStyleBackColor = True
+        '
+        'Square
+        '
+        Me.Square.Location = New System.Drawing.Point(377, 157)
+        Me.Square.Name = "Square"
+        Me.Square.Size = New System.Drawing.Size(55, 55)
+        Me.Square.TabIndex = 11
+        Me.Square.Text = "X^2"
+        Me.Square.UseVisualStyleBackColor = True
+        '
+        'SquareRoot
+        '
+        Me.SquareRoot.Location = New System.Drawing.Point(377, 218)
+        Me.SquareRoot.Name = "SquareRoot"
+        Me.SquareRoot.Size = New System.Drawing.Size(55, 55)
+        Me.SquareRoot.TabIndex = 12
+        Me.SquareRoot.Text = "√X"
+        Me.SquareRoot.UseVisualStyleBackColor = True
+        '
+        'Seven
+        '
+        Me.Seven.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Seven.Location = New System.Drawing.Point(438, 279)
+        Me.Seven.Name = "Seven"
+        Me.Seven.Size = New System.Drawing.Size(55, 55)
+        Me.Seven.TabIndex = 13
+        Me.Seven.Text = "7"
+        Me.Seven.UseVisualStyleBackColor = True
+        '
+        'Exponent
+        '
+        Me.Exponent.Location = New System.Drawing.Point(377, 279)
+        Me.Exponent.Name = "Exponent"
+        Me.Exponent.Size = New System.Drawing.Size(55, 55)
+        Me.Exponent.TabIndex = 14
+        Me.Exponent.Text = "X^y"
+        Me.Exponent.UseVisualStyleBackColor = True
+        '
+        'Roots
+        '
+        Me.Roots.Location = New System.Drawing.Point(377, 340)
+        Me.Roots.Name = "Roots"
+        Me.Roots.Size = New System.Drawing.Size(55, 55)
+        Me.Roots.TabIndex = 15
+        Me.Roots.Text = "y√X"
+        Me.Roots.UseVisualStyleBackColor = True
+        '
+        'Zero
+        '
+        Me.Zero.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Zero.Location = New System.Drawing.Point(438, 340)
+        Me.Zero.Name = "Zero"
+        Me.Zero.Size = New System.Drawing.Size(55, 55)
+        Me.Zero.TabIndex = 16
+        Me.Zero.Text = "0"
+        Me.Zero.UseVisualStyleBackColor = True
+        '
+        'Point
+        '
+        Me.Point.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Point.Location = New System.Drawing.Point(499, 340)
+        Me.Point.Name = "Point"
+        Me.Point.Size = New System.Drawing.Size(55, 55)
+        Me.Point.TabIndex = 17
+        Me.Point.Text = "."
+        Me.Point.UseVisualStyleBackColor = True
+        '
+        'Negative
+        '
+        Me.Negative.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Negative.Location = New System.Drawing.Point(560, 340)
+        Me.Negative.Name = "Negative"
+        Me.Negative.Size = New System.Drawing.Size(55, 55)
+        Me.Negative.TabIndex = 18
+        Me.Negative.Text = "+/-"
+        Me.Negative.UseVisualStyleBackColor = True
+        '
+        'Solve
+        '
+        Me.Solve.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Solve.Location = New System.Drawing.Point(621, 340)
+        Me.Solve.Name = "Solve"
+        Me.Solve.Size = New System.Drawing.Size(55, 55)
+        Me.Solve.TabIndex = 19
+        Me.Solve.Text = "="
+        Me.Solve.UseVisualStyleBackColor = True
+        '
+        'Divide
+        '
+        Me.Divide.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Divide.Location = New System.Drawing.Point(621, 279)
+        Me.Divide.Name = "Divide"
+        Me.Divide.Size = New System.Drawing.Size(55, 55)
+        Me.Divide.TabIndex = 20
+        Me.Divide.Text = "÷"
+        Me.Divide.UseVisualStyleBackColor = True
+        '
+        'Multiply
+        '
+        Me.Multiply.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Multiply.Location = New System.Drawing.Point(621, 218)
+        Me.Multiply.Name = "Multiply"
+        Me.Multiply.Size = New System.Drawing.Size(55, 55)
+        Me.Multiply.TabIndex = 21
+        Me.Multiply.Text = "x"
+        Me.Multiply.UseVisualStyleBackColor = True
+        '
+        'Minus
+        '
+        Me.Minus.Font = New System.Drawing.Font("Palatino Linotype", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Minus.Location = New System.Drawing.Point(621, 157)
+        Me.Minus.Name = "Minus"
+        Me.Minus.Size = New System.Drawing.Size(55, 55)
+        Me.Minus.TabIndex = 22
+        Me.Minus.Text = "-"
+        Me.Minus.UseVisualStyleBackColor = True
+        '
+        'Clear
+        '
+        Me.Clear.Location = New System.Drawing.Point(377, 96)
+        Me.Clear.Name = "Clear"
+        Me.Clear.Size = New System.Drawing.Size(116, 55)
+        Me.Clear.TabIndex = 23
+        Me.Clear.Text = "Clear"
+        Me.Clear.UseVisualStyleBackColor = True
+        '
+        'Backspace
+        '
+        Me.Backspace.Location = New System.Drawing.Point(499, 96)
+        Me.Backspace.Name = "Backspace"
+        Me.Backspace.Size = New System.Drawing.Size(116, 55)
+        Me.Backspace.TabIndex = 24
+        Me.Backspace.Text = "<---"
+        Me.Backspace.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(993, 446)
+        Me.Controls.Add(Me.Backspace)
+        Me.Controls.Add(Me.Clear)
+        Me.Controls.Add(Me.Minus)
+        Me.Controls.Add(Me.Multiply)
+        Me.Controls.Add(Me.Divide)
+        Me.Controls.Add(Me.Solve)
+        Me.Controls.Add(Me.Negative)
+        Me.Controls.Add(Me.Point)
+        Me.Controls.Add(Me.Zero)
+        Me.Controls.Add(Me.Roots)
+        Me.Controls.Add(Me.Exponent)
+        Me.Controls.Add(Me.Seven)
+        Me.Controls.Add(Me.SquareRoot)
+        Me.Controls.Add(Me.Square)
+        Me.Controls.Add(Me.Three)
+        Me.Controls.Add(Me.Six)
+        Me.Controls.Add(Me.Nine)
+        Me.Controls.Add(Me.Eight)
+        Me.Controls.Add(Me.Four)
+        Me.Controls.Add(Me.Five)
+        Me.Controls.Add(Me.Two)
+        Me.Controls.Add(Me.One)
+        Me.Controls.Add(Me.Plus)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Name = "Form1"
@@ -57,4 +331,27 @@ Partial Class Form1
 
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Plus As Button
+    Friend WithEvents One As Button
+    Friend WithEvents Two As Button
+    Friend WithEvents Five As Button
+    Friend WithEvents Four As Button
+    Friend WithEvents Eight As Button
+    Friend WithEvents Nine As Button
+    Friend WithEvents Six As Button
+    Friend WithEvents Three As Button
+    Friend WithEvents Square As Button
+    Friend WithEvents SquareRoot As Button
+    Friend WithEvents Seven As Button
+    Friend WithEvents Exponent As Button
+    Friend WithEvents Roots As Button
+    Friend WithEvents Zero As Button
+    Friend WithEvents Point As Button
+    Friend WithEvents Negative As Button
+    Friend WithEvents Solve As Button
+    Friend WithEvents Divide As Button
+    Friend WithEvents Multiply As Button
+    Friend WithEvents Minus As Button
+    Friend WithEvents Clear As Button
+    Friend WithEvents Backspace As Button
 End Class
